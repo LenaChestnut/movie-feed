bbd65cb689f8676d42e87a7ee23e78367a8ce62b
Elena Kashtanova <lenachestnut@gmail.com>
2021-01-19 19:52:52 +0300

    Add infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import MovieList from './components/MovieList';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
+import MovieItem from './components/MovieItem';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [ isDataLoaded, setIsDataLoaded ] = useState(false);
-  const [ moviesData, setMoviesData ] = useState({});
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ moviesData, setMoviesData ] = useState({movies: [], page: 1});
   const [ configData, setConfigData ] = useState(null);
   const BASE_URL = 'https://api.themoviedb.org/3/';
   const API_KEY = process.env.REACT_APP_API_KEY;
@@ -20,8 +21,8 @@ function App() {
     }
   }
   
-  async function getMovies(baseUrl, key) {
-    let url = `${baseUrl}discover/movie?api_key=${key}&language=ru-RU`;
+  async function getMovies(baseUrl, key, page = 1) {
+    let url = `${baseUrl}discover/movie?api_key=${key}&language=ru-RU&page=${page}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -35,17 +36,30 @@ function App() {
     (async () => {
       const movies = await getMovies(BASE_URL, API_KEY);
       const configuration = await getConfiguration(BASE_URL, API_KEY);
-      setMoviesData(movies);
+      setMoviesData({movies: [...moviesData.movies, ...movies.results], page: movies.page, totalPages: movies.total_pages});
       setConfigData(configuration);
-      setIsDataLoaded(true);
+      setIsLoading(false);
     })();
   }, []);
 
   return (
     <div className="App">
-      {isDataLoaded
-        ? <MovieList movies={moviesData.results} imgBaseUrl={configData.secure_base_url}/>
-        : <p className="loading-message">Loading...</p>}
+      {isLoading
+        ? <p className="loading-message">Loading...</p>
+        : <InfiniteScroll
+            dataLength={moviesData.movies.length}
+            next={() => {
+              getMovies(BASE_URL, API_KEY, moviesData.page + 1)
+                .then(movies => setMoviesData({...moviesData, movies: [...moviesData.movies, ...movies.results], page: movies.page}));
+            }}
+            hasMore={moviesData.page < moviesData.totalPages ? true : false}
+            className="movie-list"
+          >
+            {moviesData.movies.map((movie) => (
+              <MovieItem movieInfo={{...movie, posterUrl: `${configData.secure_base_url}w185${movie.poster_path}`}} key={uuidv4()} />
+            ))}s
+          </InfiniteScroll>
+      }
     </div>
   );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,15 @@
 import MovieItem from "./MovieItem";
 import { v4 as uuidv4 } from 'uuid';
+// import InfiniteScroll from 'react-infinite-scroll-component';
 
-function MovieList({movies, imgBaseUrl}) {
+function MovieList({movies, imgBaseUrl, getMovies}) {
     return (
         <ul className="movie-list">
+            {/* <InfiniteScroll
+                dataLength={movies.length}
+                next={() => {getMovies()}}
+            >
+            </InfiniteScroll> */}
             {movies.map((movie) => {
                 return <MovieItem movieInfo={{...movie, posterUrl: `${imgBaseUrl}w185${movie.poster_path}`}} key={uuidv4()} />;
             })}
