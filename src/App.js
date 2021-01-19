deb36bdd8f6c9f5310a4aecc0192f266e8abe718
Elena Kashtanova <lenachestnut@gmail.com>
2021-01-19 14:14:27 +0300

    Fetch data on request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [ isDataLoaded, setIsDataLoaded ] = useState(false);
+  const [ moviesData, setMoviesData ] = useState({});
+  const [ configData, setConfigData ] = useState(null);
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  
+  async function getConfiguration(baseUrl, key) {
+    let url = `${baseUrl}configuration?api_key=${key}&language=ru-RU`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  async function getMovies(baseUrl, key) {
+    let url = `${baseUrl}discover/movie?api_key=${key}&language=ru-RU`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const movies = await getMovies(BASE_URL, API_KEY);
+      const configuration = await getConfiguration(BASE_URL, API_KEY);
+      setMoviesData(movies);
+      setConfigData(configuration);
+      setIsDataLoaded(true);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {isDataLoaded
+        ? <p>Done</p>
+        : <p>Loading...</p>}
     </div>
   );
 }
